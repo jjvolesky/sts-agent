122f87b14ea4f3ca849c4d76539836de19bdcf57
nathanorwick <nathanorwick22@gmail.com>
2026-04-08 12:55:29 -0600

    probably unecessary script

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source ./venv/bin/activate
-
 python sts2-cli/python/play.py --lang en
 
-# run our agent after the game is running?
+# no idea how we'll interact yet from ours or if we'll actually run this from ours
