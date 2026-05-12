1ca884b669dd330dddfa2976bf2b8ccc64c27dd6
jjvolesky <60017217+jjvolesky@users.noreply.github.com>
2026-05-12 14:24:46 -0600

    init

--- sts_agent/pathing.py
+++ sts_agent/pathing.py
@@ -1,2 +1,34 @@
-def calculate_node_value():
+def calculate_node_value(node: dict, state: dict):
+    match node["type"]:
+        case "Monster":
+            return combat_value(node, state)
+        case "Elite":
+            return elite_value(node, state)
+        case "RestSite":
+            return rest_value(node, state)
+        case "Shop":
+            return shop_value(node, state)
+        case "Treasure":
+            return 0
+        case "Boss":
+            return 0
+        case "Ancient":
+            return 0
+        case _:
+            raise ValueError(f"Unknown room type: {node["type"]}")
+
+
+def combat_value(node, start):
+    return
+
+
+def elite_value(node, state):
+    return
+
+
+def rest_value(node, state):
+    return
+
+
+def shop_value(node, state):
     return
